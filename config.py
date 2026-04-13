777a9ee09204f8c38b04aa4b38d692035a0ae7a9
Lavish <lavishgoyal0209@gmail.com>
2026-04-13 16:32:11 +0530

    Added config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+# Thyro configuration File
+
+ASSISTANT_NAME = "Thyro"
+VERSION = "1.0.0"
+AUTHOR = "Lavish"
+
+# Music folder path
+
+MUSIC_FOLDER = r"C:\Users\Lenovo\Desktop\AI_ROBO\music"
+
+# Websites shortcuts
+
+WEBSITES = {
+    "youtube" : "https://www.youtube.com/watch?v=az-BBwW7fyk" ,
+    "google"  : "https://www.quora.com/How-can-a-man-become-a-god" ,
+    "whatsapp" : "https://www.whatsapp.com" ,
+    "github"   : "https://github.com/lavish51b1?tab=repositories" ,
+    "spotify"  : "https://open.spotify.com/track/5IXl8VmP5j38FtLIW5bWlP" ,
+    "linkedin" : "https://www.linkedin.com/in/lavish-goyal-bba8642ab/"
+}
